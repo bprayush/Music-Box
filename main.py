6cb7255a44f4a518dee2a8b6f296287474f4abc9
BugTheDebugger <bprayush@yahoo.com>
2020-02-29 21:47:55 +0545

    Working on on 3D Design for the music box

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 
 pygame.init()
-pygame.mixer.music.load('midi/Yellow.mp3')
+pygame.mixer.music.load('audio/Yellow.mp3')
 pygame.mixer.music.play()
 while pygame.mixer.music.get_busy():
     pygame.time.wait(1000)
