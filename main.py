cbcb2e1a86e23bcdb8c9e568665e1d6dfe80dab3
BugTheDebugger <bprayush@yahoo.com>
2020-03-01 18:01:47 +0545

    Adding button ...

--- main.py
+++ main.py
@@ -3,19 +3,33 @@ import time
 from audio_player import AudioPlayer
 from threading import Thread
 from crank_handler import CrankHandler
+import RPi.GPIO as GPIO
 
 audio_player = None
 crank_handler = None
 timer = 2
+button_pin = 18
 
 
 def setup():
     global audio_player
     global crank_handler
+    global button_pin
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
     audio_player = AudioPlayer()
     audio_player.load_file()
     crank_handler = CrankHandler()
+
+
+def check_button_input():
+    global button_pin
+
+    while True:
+        if GPIO.input(button_pin):
+            print("Button state: ON")
 
 
 def loop():
@@ -43,6 +57,10 @@ if __name__ == '__main__':
     try:
         setup()
         loop_thread = Thread(target=loop)
+        button_thread = Thread(target=check_button_input)
+
         loop_thread.start()
+        button_thread.start()
     except KeyboardInterrupt:
+        GPIO.cleanup()
         exit()
